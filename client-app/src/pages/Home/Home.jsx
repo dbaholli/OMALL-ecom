6df6b1f2796f792dba09dec5f0b6d4caed7deb72
diellbahollii <diell.w40@gmail.com>
2022-12-11 21:45:54 +0100

    added routing, install dependencies, remove extra boilerplate

---
+++ client-app/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "../../components/Home/Banner";
+
+const HomePage = () => {
+  return (
+    <>
+      <Banner />
+    </>
+  );
+};
+
+export default HomePage;
