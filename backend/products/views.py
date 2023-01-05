21a77f23d90b2d0fec14172109a7eded779bd1b7
Dion Berisha <diionberisha@gmail.com>
2023-01-06 00:06:06 +0100

    Created views for listing all products, and for listing products based on category

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,28 @@
+from categories.models import Categories
+from categories.serializer import CategoriesSerializer
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Product
+from .serializer import ProductSerializer
+
+
+@api_view(['GET'])
+def get_product(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['GET'])
+def get_product_by_category(request,slug):
+    if request.method == 'GET':
+        category = Categories.objects.filter(slug=slug).values('id').get()
+        id = category['id']
+        products = Product.objects.filter(category=id)
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
