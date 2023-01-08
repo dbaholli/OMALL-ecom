02523cbf18e097b204a25230fe56a527538a2f29
dbaholli <diell.w40@gmail.com>
2023-01-08 05:07:47 +0100

    add validation to forms, create register reducer and actions, send request to api

--- client-app/src/reducers/userReducers.js
+++ client-app/src/reducers/userReducers.js
@@ -3,7 +3,26 @@ import {
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAIL,
   USER_LOGOUT,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+
+    case USER_REGISTER_SUCCESS:
+      return { loading: true, userInfo: action.payload };
+
+    case USER_REGISTER_FAIL:
+      return { loading: true };
+
+    default:
+      return state;
+  }
+};
 
 export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
