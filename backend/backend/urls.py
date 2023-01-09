d474138437c3c51213325b8f37062e9d39668020
Dion Berisha <diionberisha@gmail.com>
2023-01-09 01:08:42 +0100

    Added main coupon url and connected them

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path("token/", MyTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("contactus/", include("contact_us.urls")),
-
+    path("coupon/", include("coupons.urls")),
 ]
 
 
